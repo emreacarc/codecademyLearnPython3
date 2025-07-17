2943fe940f0a80dd5820c9d47bfbaaa3d954e8d4
Emre Açar <acarem@outlook.com>
2025-07-17 12:27:07 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -63,4 +63,12 @@ inventory.insert(10, "19th Century Bed Frame")
 inventory = sorted(inventory)
 print(inventory)
 
+# Combining Lists
+owners = ["Jenny", "Alexus", "Sam", "Grace"]
+dogs_names = ["Elphonse", "Dr. Doggy DDS", "Carter", "Ralph"]
+
+names_and_dogs_names = zip(owners, dogs_names)
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+
+print(list_of_names_and_dogs_names)
 
