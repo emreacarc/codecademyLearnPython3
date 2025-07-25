37bb9d34798bc2c1f7872bd41ae8f2449105cd25
Emre Açar <acarem@outlook.com>
2025-07-25 17:29:27 +0300

    Update codeChallengesControlFlow.py

--- codeChallengesControlFlow.py
+++ codeChallengesControlFlow.py
@@ -1,1 +1,66 @@
+# ## Over Budget
+# Monthly budget
+budget = 2000
 
+# Monthly expenses
+food_bill = 200
+electricity_bill = 100
+internet_bill = 60
+rent = 1500
+
+# Calculate the total amount of expenses
+total = food_bill + electricity_bill + internet_bill + rent
+
+# Check if the total is greater than the budget and store the result in over_budget
+if total > budget:
+  over_budget = True
+else: 
+  over_budget = False
+
+# Uncomment the below lines to see the results
+print("Total: " + str(total))
+print("Is it over budget? " + str(over_budget))
+
+# ## Large Power
+# Write your large_power function here:
+def large_power(base, exponent):
+  if base ** exponent > 5000:
+    return True
+  else:
+    return False
+
+# Uncomment these function calls to test your large_power function:
+print(large_power(2, 13))
+# should print True
+print(large_power(2, 12))
+# should print False
+
+# ## Twice As Large
+# Write your twice_as_large function here:
+def twice_as_large(num1, num2):
+  if num1 > num2 * 2:
+    return True
+  else:
+    return False
+
+# Uncomment these function calls to test your twice_as_large function:
+print(twice_as_large(10, 5))
+# should print False
+print(twice_as_large(11, 5))
+# should print True
+
+# ## Divisible By Ten
+# Write your divisible_by_ten() function here:
+def divisible_by_ten(num):
+  if num % 10 == 0:
+    return True
+  else: 
+    return False
+
+
+# Uncomment these print() function calls to test your divisible_by_ten() function:
+
+print(divisible_by_ten(20))
+# should print True
+print(divisible_by_ten(25))
+# should print False
