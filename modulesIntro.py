fdaa9bebb402f136fec485ea2fe09bd758700c69
Emre Açar <acarem@outlook.com>
2025-08-07 22:25:04 +0300

    Update modulesIntro.py

--- modulesIntro.py
+++ modulesIntro.py
@@ -13,6 +13,39 @@ random_list = [random.randint(1 , 100) for i in range(101)]
 randomer_number = random.choice(random_list)
 print(randomer_number)
 
+import codecademylib3_seaborn
+# Add your code below:
+from matplotlib import pyplot as plt
+import random
+
+numbers_a = range(1, 13)
+numbers_b = random.sample(range(1000), 12)
+plt.plot(numbers_a , numbers_b)
+plt.show()
+
+# Import Decimal below:
+from decimal import Decimal
+
+# Fix the floating point math below:
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal("0.53") * Decimal("0.65")
+print(four_decimal_points)
+
+
+# Import library below:
+from library import always_three
+
+# Call your function below:
+always_three()
+
+
+
+
+
+
+
 
 
 
