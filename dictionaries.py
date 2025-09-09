576f5b313058644ac6933dcf13720dbc9ca71173
Emre Açar <acarem@outlook.com>
2025-09-09 16:59:47 +0300

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,17 @@
 translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch" }
-
 children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+#Animals in Zoo
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
+
+
+
+
