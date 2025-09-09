1d39a1f298964f1a765765977166cc2f86cc865e
Emre Açar <acarem@outlook.com>
2025-09-09 17:08:56 +0300

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -12,6 +12,18 @@ user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 print(user_ids)
 
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks, caffeine)
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+print(drinks_to_caffeine)
+
+
 
 
 
