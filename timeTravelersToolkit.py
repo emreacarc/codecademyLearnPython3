570bc67610c13fc64387c7dab919b6e3c2d9e9d8
Emre Açar <acarem@outlook.com>
2025-09-09 14:48:57 +0300

    Update timeTravelersToolkit.py

--- timeTravelersToolkit.py
+++ timeTravelersToolkit.py
@@ -2,20 +2,17 @@ from datetime import datetime as dt
 from decimal import Decimal
 from random import randint, choice
 import custom_module
+from custom_module import generate_time_travel_message
 
 current_time = dt.now()
 print(dt.strftime(current_time, "%d %b, %Y"))
-
-target_year = 2011
+possible_destinations = ["Budapest", "Prague", "Dublin", "Edinburgh", "Antalya"]
+target_destination = choice(possible_destinations)
+target_year = randint(1993,2015)
 multiplier_by_year = 275
 base_cost = Decimal("550")
 delta_year = current_time.year - target_year
 final_cost = base_cost + (delta_year * multiplier_by_year)
-print(final_cost)
 
-
-
-
-####
-def generate_time_travel_message(year, destination, cost):
-  return "Your time travel destination is {}. And the year you are going is {}. Lastly total cost you will pay: {}".format(destination, year, cost)
+message = generate_time_travel_message(target_year, target_destination, final_cost)
+print(message)
