742f79189d8b69e07229b5a821fe87653c27806c
Emre Açar <acarem@outlook.com>
2025-09-09 16:47:08 +0300

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,3 @@
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch" }
+
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
