b80363fb20a7df2911e205380bdf010904fc4194
Emre Açar <acarem@outlook.com>
2025-09-18 17:28:58 +0300

    Update hacking_the_fender.py

--- hacking_the_fender.py
+++ hacking_the_fender.py
@@ -1,1 +1,15 @@
+import csv
 
+compromised_users = []
+
+with open("passwords.csv") as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+     #print(password_row["Username"])
+    compromised_users.append(password_row["Username"])
+
+print(compromised_users)
+
+with open("compromised_users.txt", "w") as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user + "\n")
